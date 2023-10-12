a1358197ac1d187b873e559571251f98901f7530
Andy Walker <andy.walker@itron.com>
2023-10-12 12:19:56 -0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { AgGridModule } from 'ag-grid-angular';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AgGridModule, HttpClientModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
